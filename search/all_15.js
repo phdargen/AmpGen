af8bf21c110c074fff5465bd8c94bffdb7423991
Travis CI <travis@travis-ci.org>
2019-08-21 16:01:16 +0000

    Deploy code docs to GitHub Pages Travis build: 87
    Commit: b3e5466280f10e9d87c8d541facd8c4eb28bb93d

--- search/all_15.js
+++ search/all_15.js
@@ -7,7 +7,7 @@ var searchData=
   ['unregister',['unregister',['../d3/d7e/class_amp_gen_1_1_minuit_parameter_set.html#a31ed7d0a9a411fa2fc606258e63550a7',1,'AmpGen::MinuitParameterSet']]],
   ['update',['update',['../d9/dd0/class_amp_gen_1_1_binned_integrator.html#a330ace9d086fc099d20ff274dcde08ed',1,'AmpGen::BinnedIntegrator::update()'],['../de/d24/class_amp_gen_1_1_minuit_proxy.html#af2eaebaaf189d862f99f411da14e8e1f',1,'AmpGen::MinuitProxy::update()']]],
   ['updatecache',['updateCache',['../db/d4b/class_amp_gen_1_1_event_list.html#aaab4c8904d5e20ae892c4e8e83ec495c',1,'AmpGen::EventList']]],
-  ['updatenorms',['updateNorms',['../d1/d91/class_amp_gen_1_1_coherent_sum.html#aedb52307553690261ce3f93a43544063',1,'AmpGen::CoherentSum']]],
+  ['updatenorms',['updateNorms',['../d1/d91/class_amp_gen_1_1_coherent_sum.html#aedb52307553690261ce3f93a43544063',1,'AmpGen::CoherentSum::updateNorms()'],['../df/d84/class_correlated_sum.html#a7d1edcb2feaa56adc70d042ee83b77a3',1,'CorrelatedSum::updateNorms()']]],
   ['updaterunningwidth',['updateRunningWidth',['../da/d3d/class_amp_gen_1_1_three_body_calculator.html#a51b556f02f4ca325cfbbda299b680a1e',1,'AmpGen::ThreeBodyCalculator']]],
   ['utilities_2eh',['Utilities.h',['../db/d72/_utilities_8h.html',1,'']]]
 ];
