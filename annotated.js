762b9117cab9acb4037abc3d203f42bc4e64216d
Travis CI <travis@travis-ci.org>
2019-08-27 13:55:42 +0000

    Deploy code docs to GitHub Pages Travis build: 99
    Commit: ec6c40dd2a586d0574560eed1026f43a8e84cea6

--- annotated.js
+++ annotated.js
@@ -1,4 +1,12 @@
 var annotated =
 [
-    [ "AmpGen", "dc/d82/namespace_amp_gen.html", "dc/d82/namespace_amp_gen" ]
+    [ "AmpGen", "dc/d82/namespace_amp_gen.html", "dc/d82/namespace_amp_gen" ],
+    [ "CorrelatedSum", "df/d84/class_correlated_sum.html", "df/d84/class_correlated_sum" ],
+    [ "DTEvent", "db/d08/struct_d_t_event.html", "db/d08/struct_d_t_event" ],
+    [ "DTEventList", "d7/daa/struct_d_t_event_list.html", "d7/daa/struct_d_t_event_list" ],
+    [ "DTYieldCalculator", "d8/d89/class_d_t_yield_calculator.html", "d8/d89/class_d_t_yield_calculator" ],
+    [ "FixedLibPdf", "d9/da6/class_fixed_lib_pdf.html", "d9/da6/class_fixed_lib_pdf" ],
+    [ "ModelStore", "d5/dc6/struct_model_store.html", "d5/dc6/struct_model_store" ],
+    [ "normalised_pdf", "d5/de1/structnormalised__pdf.html", "d5/de1/structnormalised__pdf" ],
+    [ "Psi3770", "d8/d85/class_psi3770.html", "d8/d85/class_psi3770" ]
 ];
--- search/all_2.js
+++ search/all_2.js
@@ -7,6 +7,7 @@ var searchData=
   ['basisspinor',['basisSpinor',['../dc/d82/namespace_amp_gen.html#accb910c903aa4d36ff43121b6fc53f58',1,'AmpGen']]],
   ['basisvector',['basisVector',['../dc/d82/namespace_amp_gen.html#a73ca2984b05feaa7e2cfc7f6fb1acf11',1,'AmpGen']]],
   ['begin',['begin',['../dc/d42/class_amp_gen_1_1_bin_d_t.html#a72ab9cd89c7ef7ede96fe875ac90d56d',1,'AmpGen::BinDT::begin()'],['../d6/d9d/class_amp_gen_1_1_dynamic_container.html#a44c9929a7f872c60c4e13cdf09209321',1,'AmpGen::DynamicContainer::begin()'],['../db/d4b/class_amp_gen_1_1_event_list.html#a334c82e82a17e98d858bb605e17a2155',1,'AmpGen::EventList::begin()'],['../db/d4b/class_amp_gen_1_1_event_list.html#af26201c59958d9669e713e9804e36559',1,'AmpGen::EventList::begin() const '],['../d3/d7e/class_amp_gen_1_1_minuit_parameter_set.html#a2fde171f0a6fefdaaf02850a076730b9',1,'AmpGen::MinuitParameterSet::begin()'],['../d3/d7e/class_amp_gen_1_1_minuit_parameter_set.html#a7408a8c42fadb3f3b5e9e5589e764031',1,'AmpGen::MinuitParameterSet::begin() const '],['../d8/d73/class_amp_gen_1_1_options_parser.html#ade24ea661e78a7bea0790abce942d967',1,'AmpGen::OptionsParser::begin()'],['../d8/d73/class_amp_gen_1_1_options_parser.html#a737f62584431743f0650a430fc4c63b4',1,'AmpGen::OptionsParser::begin() const '],['../de/d6a/class_amp_gen_1_1_particle_properties_list.html#a4705e8c63fbca98b485c320273dc3376',1,'AmpGen::ParticlePropertiesList::begin()'],['../d0/dd5/class_amp_gen_1_1_transform_sequence.html#add6270e34acafafbbb23cf26c1bcbafe',1,'AmpGen::TransformSequence::begin() const '],['../d0/dd5/class_amp_gen_1_1_transform_sequence.html#aaf3c2c94bc3dc0c3edea0f46a872256e',1,'AmpGen::TransformSequence::begin()'],['../dd/da6/class_amp_gen_1_1_tree_reader.html#a77a46e08079bdaa8ac6aca586464b3ce',1,'AmpGen::TreeReader::begin()']]],
+  ['bf',['bf',['../d8/d89/class_d_t_yield_calculator.html#af1bddb0f13890a19584052f7bcb50d9b',1,'DTYieldCalculator::bf(const AmpGen::EventType &amp;type) const '],['../d8/d89/class_d_t_yield_calculator.html#af1bddb0f13890a19584052f7bcb50d9b',1,'DTYieldCalculator::bf(const AmpGen::EventType &amp;type) const ']]],
   ['bilinears',['Bilinears',['../d1/d6c/class_amp_gen_1_1_bilinears.html#a0a8e06a19bbe8e1a520e12d33125372b',1,'AmpGen::Bilinears']]],
   ['bilinears',['Bilinears',['../d1/d6c/class_amp_gen_1_1_bilinears.html',1,'AmpGen']]],
   ['binaryfcn',['binaryFCN',['../d5/db3/class_amp_gen_1_1_expression_parser.html#ad2f67400f6f5c51f2aa364b67a5249fd',1,'AmpGen::ExpressionParser']]],
@@ -15,8 +16,8 @@ var searchData=
   ['bindt',['BinDT',['../dc/d28/class_amp_gen_1_1_bin_d_t_1_1_end_node.html#a1f6eb2875a274b537ebae2dfa159864f',1,'AmpGen::BinDT::EndNode::BinDT()'],['../d1/d9d/class_amp_gen_1_1_bin_d_t_1_1_decision.html#a1f6eb2875a274b537ebae2dfa159864f',1,'AmpGen::BinDT::Decision::BinDT()'],['../dc/d42/class_amp_gen_1_1_bin_d_t.html#a8f8876012228ea83ef7dcc2d1d6a8404',1,'AmpGen::BinDT::BinDT(const ARGS &amp;...args)'],['../dc/d42/class_amp_gen_1_1_bin_d_t.html#a1d4744387c6233d391fad4bba2b3f00d',1,'AmpGen::BinDT::BinDT(const std::vector&lt; double * &gt; &amp;addr, const ARGS &amp;...args)'],['../dc/d42/class_amp_gen_1_1_bin_d_t.html#a107d4b4c5c53bf38366826deef27a9ed',1,'AmpGen::BinDT::BinDT(const EventList &amp;events, const ARGS &amp;...args)'],['../dc/d42/class_amp_gen_1_1_bin_d_t.html#aeb3db77319e04a2b59b6a2c07b54b333',1,'AmpGen::BinDT::BinDT(const ArgumentPack &amp;args)'],['../dc/d42/class_amp_gen_1_1_bin_d_t.html#a2132f1b14ea19ea7a7a5a486fe20ad65',1,'AmpGen::BinDT::BinDT()=default']]],
   ['bindt_2eh',['BinDT.h',['../da/d2e/_bin_d_t_8h.html',1,'']]],
   ['binfunctor',['binFunctor',['../db/dc8/class_amp_gen_1_1_projection.html#aa41d45e3bebc7e05d00aaf60029cfbfc',1,'AmpGen::Projection']]],
-  ['binnedintegrator',['BinnedIntegrator',['../d9/dd0/class_amp_gen_1_1_binned_integrator.html',1,'AmpGen']]],
   ['binnedintegrator',['BinnedIntegrator',['../d9/dd0/class_amp_gen_1_1_binned_integrator.html#ac13e32e6febedde38386c3f556d59a6d',1,'AmpGen::BinnedIntegrator']]],
+  ['binnedintegrator',['BinnedIntegrator',['../d9/dd0/class_amp_gen_1_1_binned_integrator.html',1,'AmpGen']]],
   ['binnumber',['binNumber',['../dc/d28/class_amp_gen_1_1_bin_d_t_1_1_end_node.html#a40ff36ac480d808d33bde52f4c62cd63',1,'AmpGen::BinDT::EndNode']]],
   ['bispinor',['Bispinor',['../d9/d03/class_amp_gen_1_1_transform.html#a0667d58f3fe94b7485e4afa9201914eda76afc2f6d1aaab24d93aaac35d4526cb',1,'AmpGen::Transform']]],
   ['bl',['BL',['../dc/d82/namespace_amp_gen.html#a46c7f25424874fdd468e11ce5b69afe7',1,'AmpGen']]],
@@ -28,9 +29,9 @@ var searchData=
   ['boost',['Boost',['../d9/d03/class_amp_gen_1_1_transform.html#a416618421d606404b0e3ed308fbf33e5aef6c025a1db8b303075a3b80572d30e8',1,'AmpGen::Transform::Boost()'],['../dc/d82/namespace_amp_gen.html#a083459d800d145e48ad06399626e8f49',1,'AmpGen::boost()']]],
   ['boundary',['boundary',['../dc/d82/namespace_amp_gen.html#ae09de36d75ccdc77d9d5fa0e20dfd7a7',1,'AmpGen']]],
   ['buffer',['buffer',['../d6/d9d/class_amp_gen_1_1_dynamic_container.html#a93128c70bede92e6014fcf309e2db72d',1,'AmpGen::DynamicContainer']]],
-  ['bugg',['Bugg',['../d3/d48/class_amp_gen_1_1_lineshape_1_1_bugg.html#a6540b96113a904c927ea9740956a1cae',1,'AmpGen::Lineshape::Bugg']]],
   ['bugg',['Bugg',['../d3/d48/class_amp_gen_1_1_lineshape_1_1_bugg.html',1,'AmpGen::Lineshape']]],
+  ['bugg',['Bugg',['../d3/d48/class_amp_gen_1_1_lineshape_1_1_bugg.html#a6540b96113a904c927ea9740956a1cae',1,'AmpGen::Lineshape::Bugg']]],
   ['build_5fprobunnormalised',['build_probunnormalised',['../d4/d31/class_amp_gen_1_1_polarised_sum.html#a97f3f9688fed38dfe4d5a060be5430ec',1,'AmpGen::PolarisedSum']]],
-  ['bw',['BW',['../de/d96/class_amp_gen_1_1_lineshape_1_1_b_w.html#a2df8064881d943462e4d3cf3ef129a74',1,'AmpGen::Lineshape::BW']]],
-  ['bw',['BW',['../de/d96/class_amp_gen_1_1_lineshape_1_1_b_w.html',1,'AmpGen::Lineshape']]]
+  ['bw',['BW',['../de/d96/class_amp_gen_1_1_lineshape_1_1_b_w.html',1,'AmpGen::Lineshape']]],
+  ['bw',['BW',['../de/d96/class_amp_gen_1_1_lineshape_1_1_b_w.html#a2df8064881d943462e4d3cf3ef129a74',1,'AmpGen::Lineshape::BW']]]
 ];
