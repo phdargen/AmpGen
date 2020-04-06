906e5d0df1d53958e7384c984d8f20936b993b45
Travis CI <travis@travis-ci.org>
2020-04-06 12:16:37 +0000

    Deploy code docs to GitHub Pages Travis build: 250
    Commit: 4c9ac8578c8e7218f858faa9e73edca91fa7483e

--- db/d16/p_correlated_sum_8h.js
+++ db/d16/p_correlated_sum_8h.js
@@ -3,7 +3,7 @@ var p_correlated_sum_8h =
     [ "pCorrelatedSum", "d6/d81/class_amp_gen_1_1p_correlated_sum.html", "d6/d81/class_amp_gen_1_1p_correlated_sum" ],
     [ "bessel", "db/d16/p_correlated_sum_8h.html#a73c0ca1bd9276f63478ffd9c33239562", null ],
     [ "chebychev", "db/d16/p_correlated_sum_8h.html#aa0396f8a5f6dba78ec9a79ff1c3f9966", null ],
-    [ "CPPoly", "db/d16/p_correlated_sum_8h.html#a3b840e209912061c60b078bb6c3d75bf", null ],
+    [ "CPSinPoly", "db/d16/p_correlated_sum_8h.html#ab548cd58ed9c8468b4a37a2ad5fadee8", null ],
     [ "laguerre", "db/d16/p_correlated_sum_8h.html#a6ba70008935ae2fcc45d135ea8aa90ce", null ],
     [ "legendre", "db/d16/p_correlated_sum_8h.html#a83f7f7eb3f71dce913c4c30a345ff8d4", null ]
 ];
